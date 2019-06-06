1004ddd9316950b6f122b0bdde52758777330e85
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-06-06 23:23:20 +0200

    Set Up environement Client

---
+++ .history/client/src/router_20190606231914.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Router from "vue-router";
+ 
+Vue.use(Router);
+ 
+export default new Router({
+  mode: "history",
+  routes: [
+    /*{
+      path: "/",
+      name: "customers",
+      alias: "/customer",
+      component: CustomersList,
+      children: [
+        {
+          path: "/customer/:id",
+          name: "customer-details",
+          component: Customer,
+          props: true
+        }
+      ]
+    }*/
+  ]
+});
